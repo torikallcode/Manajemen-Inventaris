5e91ea2f3571e41c8b39e34c35aaa1bff6ed90df
torikallcode <torikalcontent@gmail.com>
2025-03-15 17:41:07 +0800

    modify main.go

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"inventaris/connection"
-	"inventaris/controllers"
+	"inventaris/routers"
+	"net/http"
 )
 
 func main() {
@@ -10,5 +12,8 @@ func main() {
 	// controllers.CreateUser("Akbar", "Torikal@akbar.com", 21)
 	// controllers.GetUser(1)
 	// controllers.UpdateUser(1, "Torikal Akbar")
-	controllers.DeleteUser(1)
+	// controllers.DeleteUser(1)
+	routers.SetupRouter()
+	fmt.Println("Server sedang berjalan di port :8080")
+	http.ListenAndServe(":8080", nil)
 }
